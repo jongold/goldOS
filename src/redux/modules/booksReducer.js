f7e4d83600cf220adb793a01f23fa28001b14213
Jon Gold <hello@designedbygold.com>
2016-02-26 00:08:48 +1100

    Update bookshelf

--- src/redux/modules/booksReducer.js
+++ src/redux/modules/booksReducer.js
@@ -1,7 +1,11 @@
 import { fromJS } from 'immutable';
 
 const initialState = fromJS([
+  { title: 'The War of Art' },
+  { title: 'Turning Pro' },
   { title: 'Mindfulness in Plain English' },
+  { title: 'On The Road' },
+  { title: 'Vagabonding' },
   { title: 'The Artist\'s Way' },
   { title: 'Big Magic' },
   { title: 'Deep Work' },
@@ -9,6 +13,9 @@ const initialState = fromJS([
   { title: 'Get Some Headspace' },
   { title: '10% Happier' },
   { title: 'Hardcore Zen' },
+  { title: 'Meditations' },
+  { title: 'Be Here Now' },
+  { title: 'Waking Up' },
 ]);
 
 export default function booksReducer(state = initialState) {
