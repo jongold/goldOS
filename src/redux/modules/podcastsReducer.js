3f1fa814a87297698cd938b8c75ebedea54e013f
Jon Gold <hello@designedbygold.com>
2016-04-09 18:25:05 -0700

    Update podcasts

--- src/redux/modules/podcastsReducer.js
+++ src/redux/modules/podcastsReducer.js
@@ -2,8 +2,10 @@ import { fromJS } from 'immutable';
 import { SELECT_PLAYLIST_ITEM } from './constants';
 
 const initialState = fromJS({
-  current: 'Design Details #43',
+  current: 'Design Details #114',
   library: [
+    { title: 'Does Not Compute #25', url: 'https://soundcloud.com/goldos/25-falling-with-style-feat-jon-gold', date: '29th March 2016' },
+    { title: 'Design Details #114', url: 'https://soundcloud.com/goldos/114-coffee-road-show-feat-linda-eliasen-jon-gold', date: '21st March 2016' },
     { title: 'Design Details #43', url: 'https://soundcloud.com/designdetailsfm/43-freedom-juice-feat-jon-gold', date: '15th July 2015' },
     { title: 'Hack To Start', url: 'https://soundcloud.com/hacktostart/hacktostart-episode-64-jon-gold-designer-the-grid', date: '29th Sep 2015' },
     { title: 'The Start' },
